8d1b1a5aa97918257cbb59ed22be8e687bb15f8d
Philip Andresen <Philip.Andresen@gmail.com>
2022-10-28 09:32:12 -0400

    Fix full file path not being used in cleanup

--- cleanup.js
+++ cleanup.js
@@ -4,22 +4,23 @@ import config from "config";
 export default function cleanup() {
     const dbConfig = config.get(`${process.env.PC_NAME}.dbConfig`);
     const backupConfig = config.get(`${process.env.PC_NAME}.backupConfig`);
-    const twentySixHoursMs = 26*60*60*1000
-    const yesterday = new Date( Date.now() - twentySixHoursMs);
+    const twentySixHoursMs = 26 * 60 * 60 * 1000
+    const yesterday = new Date(Date.now() - twentySixHoursMs);
 
     const existingBackups = fs.readdirSync(backupConfig.targetLocation)
         .filter(fileName => {
-            return fileName.startsWith(dbConfig.database) &&
-                (fileName.endsWith('.bak') || fileName.endsWith('.zip'));
-    });
+            return fileName.startsWith(dbConfig.database) && (fileName.endsWith('.bak') || fileName.endsWith('.zip'));
+        })
+        .map(filename => `${backupConfig.targetLocation}\\${filename}`);
     console.log(`Found ${existingBackups.length} existing backup files.`);
 
-    const oldBackups = existingBackups.filter(existingBackup => {
-        const stats = fs.statSync(existingBackup);
-        return stats.mtime < yesterday;
-    });
+    const oldBackups = existingBackups
+        .filter(existingBackup => {
+            const stats = fs.statSync(existingBackup);
+            return stats.mtime < yesterday;
+        });
 
-    console.log(`Of those, ${oldBackups.length} can be deleted due to being more than ${twentySixHoursMs/60/60/1000} hours old.`);
+    console.log(`Of those, ${oldBackups.length} can be deleted due to being more than ${twentySixHoursMs / 60 / 60 / 1000} hours old.`);
 
     if (existingBackups.length <= 2) {
         console.warn('Backups will not be deleted because that would leave fewer than two backups.')
