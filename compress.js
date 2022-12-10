368b210c741ddcc8874806ee479c96183438c841
Philip Andresen <Philip.Andresen@gmail.com>
2022-12-10 14:00:41 -0500

    Add support for including additional backup files
    Specifically AIM attachments.

--- compress.js
+++ compress.js
@@ -3,35 +3,39 @@ import archiver from "archiver";
 import {log} from "./utils.js";
 import sevenZip from "node-7z";
 import sevenZipBin from '7zip-bin';
+import {notifyError} from "./sendgridEmail.js";
 
 const sevenZipLocation = sevenZipBin.path7za;
 
 /**
  * Attempts to compress with 7zip and falls back to regular zip otherwise.
- * @param backupFullFilePath
- * @returns {Promise<void>}
+ * @param {string} backupFullFilePath The .bak file created from the SQL database being backed up
+ * @param {[string]} supplementalFiles Any additional files that will be included in the backup
+ * @returns {Promise<string>} the full path of the compressed file.
  */
-export default async function compress(backupFullFilePath) {
-    return compressSevenZip(backupFullFilePath).catch(err => {
+export default async function compress(backupFullFilePath, supplementalFiles = []) {
+    return compressSevenZip(backupFullFilePath, supplementalFiles).catch(err => {
         log(err);
+        notifyError("The 7Zip compression process failed! A legacy Zip archive is being uploaded instead. This is a problem that needs to be addressed within 24 hours of this message, or you will see this again!")
         return compressZip(backupFullFilePath);
     })
 }
 
 /**
  * Takes the file located at backupFullFilePath and puts it into a compressed 7zip archive with a password
- * @param backupFullFilePath
- * @returns {Promise<void>} the full file path of the compressed .7z file.
+ * @param {string} backupFullFilePath This is the file used to set the name of the archive, the 'principal' file.
+ * @param {[string]} supplementalFiles These are any additional files to be included in the archive
+ * @returns {Promise<string>} the full file path of the compressed .7z file.
  */
-async function compressSevenZip(backupFullFilePath) {
+async function compressSevenZip(backupFullFilePath, supplementalFiles = []) {
     const sevenZipFullPath = backupFullFilePath.replace('.bak', '.7z');
     log(`Creating .7z archive at ${sevenZipFullPath}`);
     return new Promise((resolve, reject) => {
-        const archiveStream = sevenZip.add(sevenZipFullPath, backupFullFilePath, {
+        const archiveStream = sevenZip.add(sevenZipFullPath, [backupFullFilePath, ...supplementalFiles], {
             $bin: sevenZipLocation,
             method: ['x=9'],
             password: process.env.BKP_PASS
-        })
+        });
         archiveStream.on('end', () => {
             log(`Compression completed successfully.`)
             const statsBackup = fs.statSync(backupFullFilePath);
--- main.js
+++ main.js
@@ -6,14 +6,16 @@ import uploadBackup, {cleanUpDrive, uploadLog} from "./googleDriveUpload.js";
 import cleanup from "./cleanup.js";
 import {log} from "./utils.js";
 import {notifyError} from "./sendgridEmail.js";
+import config from "config";
 // register format for archiver
 // note: only do it once per Node.js process/application, as duplicate registration will throw an error
 archiver.registerFormat('zip-encrypted', archiverZE);
 
 try {
+    const backupConfig = config.get(`${process.env.PC_NAME}.backupConfig`);
     cleanup();
     const backupFullFilePath = await backup();
-    const archiveFullFilePath = await compress(backupFullFilePath);
+    const archiveFullFilePath = await compress(backupFullFilePath, [backupConfig.attachmentsLocation]);
     await uploadBackup(archiveFullFilePath);
     await cleanUpDrive();
     await uploadLog();
